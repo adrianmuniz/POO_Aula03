de057ce65ff0fbe308618b6cdea907f421e4779b
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-20 19:15:50 -0300

    Construtor POO

---
+++ src/beans/Produto.java
@@ -0,0 +1,45 @@
+package beans;
+
+public class Produto {
+	private String descricao;
+	private String marca;
+	private float valor;
+	
+	// getters e setters
+	
+	//setters = acessar, atribuir
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	// getters = retornar
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		if (valor < 0) {
+			this.valor = 0;
+		} else {
+			this.valor = valor;
+		}
+	}
+
+	public String detalhe() {
+		return "Produto:" + descricao + " - Marca:" + marca; 
+	}
+}
---
+++ src/implementacao/ExecutarProduto.java
@@ -0,0 +1,14 @@
+package implementacao;
+
+import beans.Produto;
+
+public class ExecutarProduto {
+
+	public static void main(String[] args) {
+		System.out.println("Aula 03");
+		
+		Produto micro;
+
+	}
+
+}
